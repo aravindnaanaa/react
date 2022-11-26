72cb073d0fd95e052a43db8ec9aa1649166b1eb1
aravindnaanaa <aravindnaanaa>
2022-11-27 00:45:30 +0530

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './Header';
+import Navbar from './components/NavBar'
+import Home from './components/Home'
+import Container from './components/Container'
+import Footer from './components/Footer'
+import Header from './components/Header'
+
 
 function App() {
   return (
     <div>
-      <Header/>
+      <div className="d-grid gap-2">
+            <button variant="primary" size="lg"> My info </button>
+            </div>
+      <Navbar /> 
+      <Header />
+      <Home/>
+      <Container />
+      <Footer /> 
+      
     </div>
   );
 }
